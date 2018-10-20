9d785ccfee0b384d332164c1a4be62eeb443b50e
Terrance Rose Jr <trose618@gmail.com>
2018-10-20 21:42:09 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  badge_messages = []
+  speakers.each do |name|
+    badge_messages << badge_maker(name)
+  end
+  badge_messages
+end
+
+def assign_rooms(speakers)
+  rooms = []
+  speakers.each_with_index do |attendee, index|
+    humanized_index = index + 1
+    rooms << "Hello, #{attendee}! You'll be assigned to room #{humanized_index}!"
+  end
+  rooms
+end
+
+def printer(names)
+  speakers = batch_badge_creator(names)
+  puts speakers.flatten
+  room_assignments = assign_rooms(names)
+  puts room_assignments.flatten
+end
